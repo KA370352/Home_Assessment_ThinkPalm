ac312c8f6ddb8cf367a9588e9421e062065f1bc2
KA370352 <kathirvelcse036@gmail.com>
2025-09-16 12:37:12 +0530

    Updated Library

--- tests/libraries/RabbitMQProducerLibrary.py
+++ tests/libraries/RabbitMQProducerLibrary.py
@@ -2,6 +2,7 @@ import json
 import pika
 from robot.api.deco import keyword
 from robot.api import logger
+import RabbitMQ
 
 class RabbitMQProducerLibrary:
     """RabbitMQ producer library for Robot Framework messaging integration"""
